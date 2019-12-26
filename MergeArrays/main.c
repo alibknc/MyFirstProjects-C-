3bdd4cb762ea694736908ef132d5247e285155cc
Muhammet Ali Bakınç <58572095+alibknc@users.noreply.github.com>
2019-12-26 22:14:28 +0300

    MergeArrays
    A program that merges two separate ordered arrays into a single ordered array.
    İki ayrı sıralı diziyi tek bir sıralı dizide birleştiren program.

---
+++ MergeArrays/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int N=6,M=4,ia=0,ib=0,ic=0,i;
+    int A[]={1,3,5,7,8,9};
+    int B[]={2,6,10,13};
+    int C[10];
+
+    while(ia<N && ib<M){
+        if(A[ia]>B[ib]){
+            C[ic]=B[ib];
+            ib++;
+            ic++;
+        }else{
+            C[ic]=A[ia];
+            ia++;
+            ic++;
+        }
+    }
+    while(ib<M){
+        C[ic]=B[ib];
+        ib++;
+        ic++;
+    }
+    while(ia<N){
+        C[ic] = A[ia];
+        ia++; ic++;
+    }
+    for(i=0;i<N+M;i++){
+        printf("%d ",C[i]);
+    }
+}
